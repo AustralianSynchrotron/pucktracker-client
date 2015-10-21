fcaeaca5dc4b610cb091c800a98a693b6a778efc
RobbieClarken <robbie.clarken@gmail.com>
2015-10-21 11:34:13 +1030

    Use react-transform-hmr instead of react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack')
+
 module.exports = {
   context: __dirname + '/src',
   entry: {
@@ -8,16 +10,16 @@ module.exports = {
     filename: 'app.js',
     path: __dirname + '/dist',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader'],
-      },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.html$/, loader: 'file?name=[name].[ext]' },
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(jpg|gif|png|svg|woff2?|ttf|eot)$/, loader: 'file-loader' },
     ],
   },
